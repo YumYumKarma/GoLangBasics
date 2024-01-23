28a2c63c9a09edf2e236fae71f0d2b28f12a0a43
YumYumKarma <122473865+YumYumKarma@users.noreply.github.com>
2024-01-23 23:28:47 +0530

    updated the pointers file

--- pointers/main.go
+++ pointers/main.go
@@ -33,11 +33,12 @@ func main() {
 
 	log.Println("myString is set to", myString)
 
-	changeUsingPointer(&myString)
+	changeUsingPointer(&myString) //refrence
 	log.Println("myString is updated to",myString)
 }
 
-func changeUsingPointer(s *string) {
+func changeUsingPointer(s *string) { //pointer
+	log.Println("s is set to :", s)
 	newValue := "Red"
 	*s = newValue
 }
